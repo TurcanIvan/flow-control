7026d23270414385431dd0b969eacf4114e544b3
TurcanIvan <68420218+TurcanIvan@users.noreply.github.com>
2021-11-27 08:04:14 +0200

    Exemple

---
+++ flow-control.py
@@ -0,0 +1,30 @@
+# python FLOW CONTROL (gestionarea cu fluxul de executie)
+
+# aici starteaaza aplicatia
+# flow linear
+
+# inst1
+# inst2
+# inst3
+# inst4
+# inst5
+
+money = 1000000                                #inst1
+
+pin = input("Enter your pin code")             #inst2
+
+if pin == "1111":
+
+    print("You have :", money, "on your account")  #inst3
+
+else:
+
+    print("This pin is not corect")
+
+    #BRANCHING
+    #if,if/else,if.elif/..../else
+
+    #LOOPING
+    #while,for
+
+
